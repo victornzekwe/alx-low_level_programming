2f80c4929ad39566de094d16debde4f8e41f0ac8
victornzekwe <vicakanze@gmail.com>
2023-09-14 11:06:53 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
